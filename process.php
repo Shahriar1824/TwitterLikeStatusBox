96adc686ac6956ebb282718bdd9a545aa4414359
Shahriar1824 <SRashid1824@gmail.com>
2015-12-09 01:55:02 +0600

    Finishing the word counter
    Finished the word counter and create a warning line so user can know
    when to stop .

--- process.php
+++ process.php
@@ -12,11 +12,11 @@ if(isset($_GET['value'])){
 
     $len = strlen($chars);
 
-    echo "<h2 style='color: blueviolet; float: right'>Character write so far:- ".$len."</h2>";
+    echo "<h2 style='color: blueviolet; float: right'>Character write so far: ".$len."</h2>";
 
-    if($len>100){
+    if($len>140){
 
-        echo "<br><br><p style='color:#ff0000; font-size: 17px; '>You are crossing the limit!</p>";
+        echo "<br><br><p style='color:#ff0000; font-size: 17px;font-family: 'comic sans MS'; '>You are crossing the limit!</p>";
 
     }
 }
